1ee9745aa3b5ad1d0297cca4e508ab47445ea7b0
EmmeForte90 <mauroemme90@gmail.it>
2023-01-20 12:08:33 +0100

    Set15

--- Test/Assets/GameplayManager.cs
+++ Test/Assets/GameplayManager.cs
@@ -7,10 +7,19 @@ public class GameplayManager : MonoBehaviour
 {
     public static bool playerExists;
     
+private GameObject player; // Variabile per il player
 
+    private CinemachineVirtualCamera virtualCamera;
 
     private void Awake()
     {
+        player = GameObject.FindWithTag("Player");
+virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
+virtualCamera.Follow = player.transform;
+virtualCamera.LookAt = player.transform;
+
+
+
         // Verifica se un'istanza del GameObject esiste già e distruggila se necessario
         if (playerExists) 
         {
