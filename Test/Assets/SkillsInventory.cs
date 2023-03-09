22dddb051390db9564d4a64ce0f2d9fa0b2bca8c
EmmeForte90 <mauroemme90@gmail.it>
2023-03-09 08:39:59 +0100

    Fix Skil and inventory system

---
+++ Test/Assets/SkillsInventory.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillsInventory : MonoBehaviour
+{
+    public static SkillsInventory Instance;
+   public List<Skill> abil = new List<Skill>();
+
+   public Transform SkillContent;
+   public GameObject InventorySkill;
+
+   private void Awake()
+   {
+    Instance = this;
+   }
+
+   public void Add(Skill ab)
+   {
+    abil.Add(ab);
+   }
+
+   public void Remove(Skill ab)
+   {
+     abil.Remove(ab);
+   }
+
+   public void Listabil()
+   {
+      foreach (Transform child in SkillContent)
+      {
+         Destroy(child.gameObject);
+      }
+
+      foreach (var ab in abil)
+      {
+         GameObject obj = Instantiate(InventorySkill, SkillContent);
+         var abIcon = obj.transform.Find("skill_icon").GetComponent<Image>();
+
+         abIcon.sprite = ab.icon;
+      }
+   }
+}
+
+
+
